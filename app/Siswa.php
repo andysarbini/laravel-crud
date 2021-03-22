60b63baf505616fa3853a1e3f7ba4186f6114180
andysarbini <kangandy09@gmail.com>
2021-03-22 09:39:41 +0700

    part 18 - crud, bootstrap input nilai siswa
    by rollo academy youtube

---
+++ app/Siswa.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    protected $table = 'siswa';
+    protected $fillable = ['nama_belakang', 'nama_depan', 'jenis_kelamin', 'agama', 'alamat', 'avatar', 'user_id'];
+
+    public function getAvatar()
+    {
+        if(!$this->avatar){
+            return asset('images/default.jpg');
+        }
+        return asset('images/'.$this->avatar);
+    }
+
+    public function mapel() // php artisan tinker; utk uji coba data via terminal, misal relasi
+    {
+        return $this->belongsToMany(Mapel::class)->withPivot(['nilai']); // mengambil field nilai di tabel mapel_siswa
+    }
+}
